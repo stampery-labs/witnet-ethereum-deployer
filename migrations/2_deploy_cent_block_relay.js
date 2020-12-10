c5c634b506a959545489ca5ac642fb61215f1f6b
Gorka Irazoqui <gorka.irazoki@gmail.com>
2020-12-10 16:05:49 +0100

    update solidity version and centralized block relay deployer

--- migrations/2_deploy_cent_block_relay.js
+++ migrations/2_deploy_cent_block_relay.js
@@ -1,13 +1,14 @@
 var CentralizedBlockRelay = artifacts.require("CentralizedBlockRelay")
 const addresses = require("./addresses.json")
 
-module.exports = function (deployer, network) {
+module.exports = function (deployer, network, accounts) {
   network = network.split("-")[0]
   if (network in addresses && addresses[network].CentralizedBlockRelay) {
     CentralizedBlockRelay.address = addresses[network].CentralizedBlockRelay
   }
   else{
     console.log(`> Migrating CentralizedBlockRelay into ${network} network`)
-    deployer.deploy(CentralizedBlockRelay)
+    deployer.deploy(CentralizedBlockRelay, [accounts[0]])
   }
 }
+

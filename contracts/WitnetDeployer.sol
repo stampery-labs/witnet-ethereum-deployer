c9fd394f59847252cac6f0004294c845176a964c
Claudia Bartoli Duncan <claudia@stampery.co>
2021-05-20 11:18:23 +0200

    chore: update sol verison 0.8.4
    Signed-off-by: Claudia Bartoli Duncan <claudia@stampery.co>

--- contracts/WitnetDeployer.sol
+++ contracts/WitnetDeployer.sol
@@ -1,16 +1,13 @@
-pragma solidity ^0.6.6;
-pragma experimental ABIEncoderV2;
+pragma solidity 0.8.4;
 
 // Import the UsingWitnet library that enables interacting with Witnet
 import "witnet-ethereum-bridge/contracts/UsingWitnet.sol";
 import "witnet-ethereum-bridge/contracts/WitnetRequestBoard.sol";
 import "witnet-ethereum-bridge/contracts/WitnetRequestBoardProxy.sol";
-import "witnet-ethereum-bridge/contracts/Witnet.sol";
-import "witnet-ethereum-bridge/contracts/CBOR.sol";
 // Import price feeds
-import "witnet-price-feeds-examples/contracts/bitcoin_price_feed/BtcUsdPriceFeed.sol";
-import "witnet-price-feeds-examples/contracts/eth_price_feed/EthUsdPriceFeed.sol";
-import "witnet-price-feeds-examples/contracts/gold_price_feed/GoldEurPriceFeed.sol";
+import "witnet-price-feed-examples/contracts/BtcUsdPriceFeed.sol";
+import "witnet-price-feed-examples/contracts/EthUsdPriceFeed.sol";
+import "witnet-price-feed-examples/contracts/GoldEurPriceFeed.sol";
 
 contract WitnetDeployer  {
 }
